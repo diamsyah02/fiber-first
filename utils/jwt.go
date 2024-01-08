174d78e5c2ae2268e4f62a3da1a6be059ff060a3
Diamsyah M Dida <didadiamsyah02@gmail.com>
2024-01-08 08:47:16 +0700

    Update generate dan verify token

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,21 +1,26 @@
 package utils
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var APPLICATION_NAME = "My Simple JWT App"
-var LOGIN_EXPIRATION_DURATION = time.Duration(1) * time.Hour
+var APPLICATION_NAME = "Rest Api Fiber First"
+var LOGIN_EXPIRATION_DURATION = time.Now().Add(1 * time.Minute)
 var JWT_SIGNING_METHOD = jwt.SigningMethodHS256
-var JWT_SIGNATURE_KEY = []byte("the secret of kalimdor")
+var JWT_SIGNATURE_KEY = []byte("AC9D72E4B61B7935B28D412F72193")
 
 func GenerateToken(username string) string {
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(LOGIN_EXPIRATION_DURATION),
+		},
+	}
 	token := jwt.NewWithClaims(
 		JWT_SIGNING_METHOD,
-		jwt.MapClaims{},
+		claims,
 	)
 
 	signedToken, err := token.SignedString(JWT_SIGNATURE_KEY)
@@ -27,22 +32,21 @@ func GenerateToken(username string) string {
 }
 
 func VerifyToken(tokenString string) bool {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Signing method invalid")
-		} else if method != JWT_SIGNING_METHOD {
-			return nil, fmt.Errorf("Signing method invalid")
-		}
-
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
 		return JWT_SIGNATURE_KEY, nil
 	})
+
 	if err != nil {
 		return false
 	}
 
-	_, ok := token.Claims.(jwt.MapClaims)
-	if !ok || !token.Valid {
+	if !token.Valid {
 		return false
 	}
 	return true
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
 }
